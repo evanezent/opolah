50d2b2842134b9bb1572b273723292d4710a5fb6
evanezcent <pratamayo16@gmail.com>
2020-11-28 14:28:59 +0700

    sticky navbar

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -58,64 +58,73 @@ class _HomeScreenState extends State<HomeScreen> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          SearchBar(),
-          SizedBox(height: 20),
-          Container(
-            child: CarouselSlider.builder(
-              itemCount: 3,
-              options: CarouselOptions(
-                autoPlay: true,
-                autoPlayAnimationDuration: Duration(seconds: 2),
-                enlargeCenterPage: true,
-                viewportFraction: 0.9,
-                aspectRatio: 2.0,
-                initialPage: 1,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: [
+                  SizedBox(height: 20),
+                  Container(
+                    child: CarouselSlider.builder(
+                      itemCount: 3,
+                      options: CarouselOptions(
+                        autoPlay: true,
+                        autoPlayAnimationDuration: Duration(seconds: 2),
+                        enlargeCenterPage: true,
+                        viewportFraction: 0.9,
+                        aspectRatio: 2.0,
+                        initialPage: 1,
+                      ),
+                      itemBuilder: (context, index) => images[index],
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        CardIcon(
+                          icon: FontAwesomeIcons.tshirt,
+                          title: "Cloth",
+                        ),
+                        CardIcon(
+                          icon: FontAwesomeIcons.mobile,
+                          title: "Mobile",
+                        ),
+                        CardIcon(
+                          icon: FontAwesomeIcons.laptop,
+                          title: "Computer",
+                        ),
+                        CardIcon(
+                          icon: FontAwesomeIcons.theaterMasks,
+                          title: "Hobbies",
+                        ),
+                        CardIcon(
+                          icon: FontAwesomeIcons.book,
+                          title: "Books",
+                        ),
+                      ],
+                    ),
+                  ),
+                  SliderItem(
+                    size: size,
+                    title: "Newcomer",
+                    openMore: () {},
+                  ),
+                  SliderItem(
+                    size: size,
+                    title: "Popular",
+                    openMore: () {},
+                  )
+                ],
               ),
-              itemBuilder: (context, index) => images[index],
             ),
-          ),
-          SizedBox(height: 30),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                CardIcon(
-                  icon: FontAwesomeIcons.tshirt,
-                  title: "Cloth",
-                ),
-                CardIcon(
-                  icon: FontAwesomeIcons.mobile,
-                  title: "Mobile",
-                ),
-                CardIcon(
-                  icon: FontAwesomeIcons.laptop,
-                  title: "Computer",
-                ),
-                CardIcon(
-                  icon: FontAwesomeIcons.theaterMasks,
-                  title: "Hobbies",
-                ),
-                CardIcon(
-                  icon: FontAwesomeIcons.book,
-                  title: "Books",
-                ),
-              ],
-            ),
-          ),
-          SliderItem(
-            size: size,
-            title: "Newcomer",
-            openMore: () {},
-          ),
-          SliderItem(
-            size: size,
-            title: "Popular",
-            openMore: () {},
-          )
-        ],
+            SearchBar(),
+          ],
+        ),
       ),
     );
   }
