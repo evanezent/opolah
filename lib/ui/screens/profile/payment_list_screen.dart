43ae11b2069fb8348297d521a969ad95fa7e1050
evanezcent <pratamayo16@gmail.com>
2020-12-05 11:03:23 +0700

    payment screen done, init payment list in profile

---
+++ lib/ui/screens/profile/payment_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaymentList extends StatefulWidget {
+  @override
+  _PaymentListState createState() => _PaymentListState();
+}
+
+class _PaymentListState extends State<PaymentList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+    );
+  }
+}
