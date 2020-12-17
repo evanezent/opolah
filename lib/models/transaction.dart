ec54139aceed643b79bbef077f23414c6080cbb3
evanezcent <pratamayo16@gmail.com>
2020-12-17 23:28:18 +0700

    transaction model

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,3 +1,8 @@
+import 'package:opolah/models/address.dart';
+import 'package:opolah/models/item.dart';
+
 class Transaction {
-  String id;
+  String id, total, paymentProof;
+  Address address;
+  List<Item> items;
 }
