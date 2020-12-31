75cb39c7ec30f24f86a14584c9641e1d21f9eaf9
evanezcent <pratamayo16@gmail.com>
2020-12-31 23:16:46 +0700

    showing transactions

--- lib/ui/screens/profile/history_list_screen.dart
+++ lib/ui/screens/profile/history_list_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:opolah/blocs/transaction/transaction_bloc.dart';
+import 'package:opolah/blocs/transaction/transaction_state.dart';
+import 'package:opolah/constant/constans.dart';
 import 'package:opolah/models/transaction.dart';
 import 'package:opolah/ui/components/history_item.dart';
 import 'package:opolah/ui/screens/shipping/shipping_screen.dart';
@@ -13,34 +17,71 @@ class HistoryList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body: Container(
-        padding: EdgeInsets.all(10),
-        child: historyList.length == 0
-            ? Container()
-            : Container(
-                padding: EdgeInsets.all(10),
-                child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  itemCount: historyList.length,
-                  itemBuilder: (context, index) => HistoryItem(
-                    transactionItem: historyList[index],
-                    clickCallback: () {},
-                    cardClicked: () {
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ShippingScreen(
-                                  viewOnly: true,
-                                  address: historyList[index].getAddress,
-                                  choosen: historyList[index].getCarts,
-                                  totalItemPrice: int.parse(
-                                          historyList[index].getTotal) -
-                                      int.parse(historyList[index].getCost))));
-                    },
-                  ),
+      body: BlocBuilder<TransactionBloc, TransactionState>(
+        builder: (context, state) {
+          if (state is TransactionLoading) {
+            return Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.white,
+                valueColor: AlwaysStoppedAnimation(colorPrimary),
+              ),
+            );
+          } else if (state is TransactionSuccessLoad) {
+            final histories =
+                state.transctionList.where((data) => data.getStatus).toList();
+                
+            return Container(
+              padding: EdgeInsets.all(10),
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: histories.length,
+                itemBuilder: (context, index) => HistoryItem(
+                  transactionItem: histories[index],
+                  clickCallback: () {},
+                  cardClicked: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ShippingScreen(
+                                viewOnly: true,
+                                address: histories[index].getAddress,
+                                choosen: histories[index].getCarts,
+                                totalItemPrice:
+                                    int.parse(histories[index].getTotal) -
+                                        int.parse(histories[index].getCost))));
+                  },
                 ),
               ),
+            );
+          }
+          return Container();
+        },
       ),
+      // historyList.length == 0
+      //     ? Container()
+      //     : Container(
+      //         padding: EdgeInsets.all(10),
+      //         child: ListView.builder(
+      //           scrollDirection: Axis.vertical,
+      //           itemCount: historyList.length,
+      //           itemBuilder: (context, index) => HistoryItem(
+      //             transactionItem: historyList[index],
+      //             clickCallback: () {},
+      //             cardClicked: () {
+      //               Navigator.pushReplacement(
+      //                   context,
+      //                   MaterialPageRoute(
+      //                       builder: (context) => ShippingScreen(
+      //                           viewOnly: true,
+      //                           address: historyList[index].getAddress,
+      //                           choosen: historyList[index].getCarts,
+      //                           totalItemPrice: int.parse(
+      //                                   historyList[index].getTotal) -
+      //                               int.parse(historyList[index].getCost))));
+      //             },
+      //           ),
+      //         ),
+      //       ),
     );
   }
 }
--- lib/ui/screens/profile/payment_list_screen.dart
+++ lib/ui/screens/profile/payment_list_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:opolah/blocs/transaction/transaction_bloc.dart';
+import 'package:opolah/blocs/transaction/transaction_state.dart';
+import 'package:opolah/constant/constans.dart';
 import 'package:opolah/models/transaction.dart';
 import 'package:opolah/ui/components/history_item.dart';
 import 'package:opolah/ui/screens/payment/payment_screen.dart';
@@ -12,29 +16,69 @@ class PaymentList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: transactionList.length == 0
-          ? Container()
-          : Container(
-              padding: EdgeInsets.all(10),
-              child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                itemCount: transactionList.length,
-                itemBuilder: (context, index) => HistoryItem(
-                  transactionItem: transactionList[index],
-                  tab: 'payment',
-                  clickCallback: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => PaymentScreen(
-                                total: double.parse(
-                                    transactionList[index].getTotal),
-                                transactionID: transactionList[index].getID)));
-                  },
+        backgroundColor: Colors.grey[200],
+        body: BlocBuilder<TransactionBloc, TransactionState>(
+          builder: (context, state) {
+            if (state is TransactionLoading) {
+              return Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.white,
+                  valueColor: AlwaysStoppedAnimation(colorPrimary),
                 ),
-              ),
-            ),
-    );
+              );
+            } else if (state is TransactionSuccessLoad) {
+              final transactions = state.transctionList
+                  .where((data) => data.getProof == "")
+                  .toList();
+
+              return Container(
+                padding: EdgeInsets.all(10),
+                child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: transactions.length,
+                    itemBuilder: (context, index) {
+                      return HistoryItem(
+                        transactionItem: transactions[index],
+                        tab: 'payment',
+                        clickCallback: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => PaymentScreen(
+                                      total: double.parse(
+                                          transactions[index].getTotal),
+                                      transactionID:
+                                          transactions[index].getID)));
+                        },
+                      );
+                    }),
+              );
+            }
+            return Container();
+          },
+        )
+        // transactionList.length == 0
+        //     ? Container()
+        //     : Container(
+        //         padding: EdgeInsets.all(10),
+        //         child: ListView.builder(
+        //           scrollDirection: Axis.vertical,
+        //           itemCount: transactionList.length,
+        //           itemBuilder: (context, index) => HistoryItem(
+        //             transactionItem: transactionList[index],
+        //             tab: 'payment',
+        //             clickCallback: () {
+        //               Navigator.push(
+        //                   context,
+        //                   MaterialPageRoute(
+        //                       builder: (context) => PaymentScreen(
+        //                           total: double.parse(
+        //                               transactionList[index].getTotal),
+        //                           transactionID: transactionList[index].getID)));
+        //             },
+        //           ),
+        //         ),
+        //       ),
+        );
   }
 }
